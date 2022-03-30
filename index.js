d9fd2370775b3a7bd028461b231ce1a34121c78d
Piero <matematicadivina1@gmail.com>
2022-03-29 19:53:52 -0700

    sad

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
+import start from "./src/services/api.js"
 
-import fastify from 'fastify'
-import Person from './src/models/Persona.js'
-import start from './src/services/api.js'
-import conn from './src/services/database.js'
-
-
-export const fast = fastify({logger:true})
-
-fast.get('/', async (request,reply)=>{
-    return 'Bye mundo'
-})
-
-fast.post('/api/person', async (request, reply)=>{
-    try {
-        const conexion = await conn()
-        const data = request.body
-        const person = new Person(data)
-        await person.save()
-        conexion.close()
-        return data
-    } catch (error) {
-        fast.log.error(error)
-        return error
-    }
-    
-})
 
 start()
---
+++ src/routes/endpoint.js
@@ -0,0 +1,24 @@
+import fastify from 'fastify'
+import Person from '../models/Persona.js'
+import conn from '../services/database.js'
+
+export const fast = fastify({logger:true})
+
+fast.get('/', async (request,reply)=>{
+    return 'Bye mundo'
+})
+
+fast.post('/api/person', async (request, reply)=>{
+    try {
+        const conexion = await conn()
+        const data = request.body
+        const person = new Person(data)
+        await person.save()
+        conexion.close()
+        return data
+    } catch (error) {
+        fast.log.error(error)
+        return error
+    }
+    
+})
